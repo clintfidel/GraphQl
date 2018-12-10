5ce980d45efd210e5135b40c326b2800c8414591
clintfidel <clintfidel@gmail.com>
2018-12-10 14:33:03 +0100

    setup complete

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-import express from express;
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import winston from 'winston'
+import { makeExecutableSchema } from 'graphql-tools'
 
 const app = express();
 const port = process.env.PORT || 5600;
 
-app.use(bodyParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const typeDefs = `
+  type Query {
+    greeting: String
+  }
+`;
 
-app.get('/api/v1', (req, res) => {
-    res.status(200).send('Welcome to WEconnect Api');
+const resolvers = {
+  Query : {
+    greeting: () => 'Hello world!'
+  }
+};
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
+app.use(bodyParser());
+app.use(cors(), bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/graphQl', graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL  : '/graphQl' }))
+app.get('*', (req, res) => {
+    res.status(200).send('Welcome to learning GraphQL');
 });
 
 
